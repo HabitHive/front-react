59fb79ef5a9a0cfe8a3dcafcfca18356d39666ca
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2022-08-28 23:33:33 +0900

    테스트 레이아웃

--- src/pages/MainDailyPage.jsx
+++ src/pages/MainDailyPage.jsx
@@ -1,5 +1,20 @@
+import styled from "styled-components";
+
 const MainDailyPage = () => {
-  return <div>너무두렵습니다....</div>;
+  return (
+    <div>
+      <StLayout>테스트 레이아웃</StLayout>
+    </div>
+  );
 };
 
 export default MainDailyPage;
+
+const StLayout = styled.div`
+  background-color: white;
+  max-width: 390px;
+  height: 100vh;
+  margin: 0 auto;
+  /* max-width: 1000px;
+  padding: 0 20px; */
+`;
