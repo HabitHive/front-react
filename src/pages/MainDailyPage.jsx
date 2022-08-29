b2bebcedfd7e6e87017b671d02b4a7cc09307956
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2022-08-30 01:07:43 +0900

    년, 월 구현 완료

--- src/pages/MainDailyPage.jsx
+++ src/pages/MainDailyPage.jsx
@@ -1,14 +1,22 @@
 import styled from "styled-components";
 
 import WeekCalendar from "../components/main/daily/WeekCalendar";
+import { useNavigate } from "react-router";
 
 const MainDailyPage = () => {
+  const navigate = useNavigate();
+
+  //먼슬리 페이지로 이동
+  const GoMonthly = () => {
+    navigate("/monthly");
+  };
+
   return (
     <>
       <Header>
         <div className="container">
           <h3>My daily</h3>
-          <button>먼슬리로 이동</button>
+          <button onClick={GoMonthly}>먼슬리로 이동</button>
         </div>
       </Header>
       <Body>
