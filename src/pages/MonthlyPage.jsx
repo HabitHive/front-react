1b991a9644c62aa023ae308b53de90d79c93553a
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2022-09-02 20:39:20 +0900

    #30 캘린더 구현하기

--- src/pages/MonthlyPage.jsx
+++ src/pages/MonthlyPage.jsx
@@ -1,5 +1,29 @@
+import MonthlyCalendar from "../components/main/monthly/MonthlyCalendar";
+import { FiChevronLeft } from "react-icons/fi";
+import styled from "styled-components";
+
 const MonthlyPage = () => {
-  return <div>먼슬리 페이지입니다</div>;
+  return (
+    <div>
+      <Header>
+        <div className="container">
+          <FiChevronLeft />
+          <span>마이 먼슬리</span>
+        </div>
+      </Header>
+      <div></div>
+      <MonthlyCalendar />
+    </div>
+  );
 };
 
 export default MonthlyPage;
+
+const Header = styled.div`
+  .container {
+    display: flex;
+    span {
+      justify-content: center;
+    }
+  }
+`;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -4,27 +4,26 @@ import LogInPage from "../pages/LogInPage";
 import SignUpPage from "../pages/SignUpPage";
 import MainDailyPage from "../pages/MainDailyPage";
 import MonthlyPage from "../pages/MonthlyPage";
-import NotFoundPage from "../pages/NotFoundPage"
+import NotFoundPage from "../pages/NotFoundPage";
 import PostingPage from "../pages/PostingPage";
 import SurveyPage from "../pages/SurveyPage";
 import MyPage from "../pages/MyPage";
 import TagBuyPage from "../pages/TagBuyPage";
 
 const Router = () => {
-
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<LogInPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/main" element={<MainDailyPage />} />
-            <Route path="/monthly" element={<MonthlyPage />} />
-            <Route path="/*" element={<NotFoundPage />} />
-            <Route path="/post" element={<PostingPage />} />
-            <Route path="/survey" element={<SurveyPage />} />
-            <Route path="/mypage" element={<MyPage />} />
-            <Route path="/buy" element={<TagBuyPage />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<LogInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/main" element={<MainDailyPage />} />
+        <Route path="/monthly" element={<MonthlyPage />} />
+        <Route path="/*" element={<NotFoundPage />} />
+        <Route path="/post" element={<PostingPage />} />
+        <Route path="/survey" element={<SurveyPage />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/buy" element={<TagBuyPage />} />
+      </Routes>
     </BrowserRouter>
   );
 };
