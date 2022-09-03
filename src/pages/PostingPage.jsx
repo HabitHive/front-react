f11f9c7cab8bb3801cd616b24b02b2a49839bba7
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2022-09-04 04:57:17 +0900

    #32 post view 초안

--- src/pages/PostingPage.jsx
+++ src/pages/PostingPage.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import styled from "styled-components";
+import Header from "../components/common/Header";
 
 let now = new Date();
 const PostingPage = () => {
@@ -12,13 +13,15 @@ const PostingPage = () => {
 
   return (
     <div>
-      <div className="headercontainer">
-        <FiChevronLeft />
-        <span>데일리 설정</span>
-      </div>
+      {/* <FiChevronLeft /> */}
+      <Header
+        text={"데일리 설정"}
+        // LeftText={<FiChevronLeft />}
+      />
       <BodyContainer>
-        <div className="tagtitle"> 운동하기 ( 30일 )</div>
-        <div className="startdate">
+        <div className="tagTitle"> 운동하기 ( 30일 )</div>
+        <div className="startDate">
+          <span>시작날짜</span>
           <DatePicker
             selected={startDate}
             onChange={(date) => setStartDate(date)}
@@ -30,16 +33,19 @@ const PostingPage = () => {
             // excludeDateIntervals={[{start: subDays(new Date(), 5), end: addDays(new Date(), 5) }]}
             // calendarStartDay={0}
           />
-          <DatePicker
-            selected={endDate}
-            onChange={(date) => setEndDate(date)}
-            selectsEnd
-            startDate={startDate}
-            endDate={endDate}
-            minDate={startDate}
-          />
+          <div className="endDate">
+            <DatePicker
+              selected={endDate}
+              onChange={(date) => setEndDate(date)}
+              selectsEnd
+              startDate={startDate}
+              endDate={endDate}
+              minDate={startDate}
+            />
+          </div>
         </div>
-        <div className="starttime">
+        <div className="startTime">
+          <span>시작시간</span>
           <DatePicker
             selected={startDate}
             onChange={(date) => setStartDate(date)}
@@ -50,37 +56,54 @@ const PostingPage = () => {
             dateFormat="h:mm aa"
           />
         </div>
-        <div className="endtime"></div>
-        <DatePicker
-          selected={endDate}
-          onChange={(date) => setEndDate(date)}
-          showTimeSelect
-          showTimeSelectOnly
-          timeIntervals={30}
-          timeCaption="Time"
-          dateFormat="h:mm aa"
-        />
-        <div className="enddate"></div>
-        <div className="repeatday"></div>
+        <div className="endTime">
+          <span>종료시간</span>
+          <DatePicker
+            selected={endDate}
+            onChange={(date) => setEndDate(date)}
+            showTimeSelect
+            showTimeSelectOnly
+            timeIntervals={30}
+            timeCaption="Time"
+            dateFormat="h:mm aa"
+          />
+        </div>
+        <div className="expireDay">
+          <span>유효기간</span>
+          <div className="expireDate"> 2022년 10월 30일까지</div>
+        </div>
+        <div className="repeatDay"></div>
       </BodyContainer>
-      <button>저장</button>
+      <Button>
+        <button>저장</button>
+      </Button>
     </div>
   );
 };
 
 export default PostingPage;
 
+const HeaderContainer = styled.div``;
+
 const BodyContainer = styled.div`
   /* justify-content: center; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-  .tagtitle {
+  & .tagTitle {
     background-color: coral;
     border-radius: 10px;
-    width: 400px;
-    height: 30px;
     display: flex;
     /* text-align: center; */
     justify-content: center;
     align-items: center;
+    width: 400px;
+    height: 30px;
   }
+`;
+
+const Button = styled.div`
+  display: flex;
+  justify-content: center;
 `;
