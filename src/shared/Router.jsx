b893c01b6d237c68a26cf7725d68488d73d9de54
Jisu Lee <jisu2281l@naver.com>
2022-09-07 04:11:59 +0900

    #13 로그인 상태 전역관리

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -11,20 +11,40 @@ import MyPage from "../pages/MyPage";
 import TagBuyPage from "../pages/TagBuyPage";
 import OnboardingPage from "../pages/OnboardingPage";
 
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import setToken from "../axios/setToken";
+
+import { setLogin } from "../redux/modules/user";
+
 const Router = () => {
+  const dispatch = useDispatch();
+
+  const isLog = useSelector(state=>state.user.isLog)
+  console.log(isLog)
+  
+  const token = localStorage.getItem("token")
+
+  useEffect(()=>{
+    setToken(token)
+    if (token) {
+      dispatch(setLogin())
+    }
+  })
+
   return (
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<LogInPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/onboarding" element={<OnboardingPage />} />
-            <Route path="/main" element={<MainDailyPage />} />
-            <Route path="/monthly" element={<MonthlyPage />} />
-            <Route path="/post" element={<PostingPage />} />
-            <Route path="/survey" element={<SurveyPage />} />
-            <Route path="/mypage" element={<MyPage />} />
-            <Route path="/buy" element={<TagBuyPage />} />
-            <Route path="/*" element={<NotFoundPage />} />
+            <Route path="/" element={isLog ? <MainDailyPage/> : <LogInPage/>}/>
+            <Route path="/signup" element={isLog ? <MainDailyPage/> : <SignUpPage/>}/>
+            <Route path="/onboarding" element={<OnboardingPage/>}/>
+            <Route path="/main" element={<MainDailyPage/>}/>
+            <Route path="/monthly" element={<MonthlyPage/>}/>
+            <Route path="/post" element={<PostingPage/>}/>
+            <Route path="/survey" element={<SurveyPage/>}/>
+            <Route path="/mypage" element={<MyPage/>}/>
+            <Route path="/buy" element={<TagBuyPage/>}/>
+            <Route path="/*" element={<NotFoundPage/>}/>
         </Routes>
     </BrowserRouter>
   );
