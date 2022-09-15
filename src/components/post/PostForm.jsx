30ebdd8cce11d32c8e31f9e9772de1905522b247
Jisu Lee <jisu2281l@naver.com>
2022-09-15 17:11:17 +0900

    에러 수정 작업

--- src/components/post/PostForm.jsx
+++ src/components/post/PostForm.jsx
@@ -1,18 +1,14 @@
 import styled from "styled-components";
 import Header from "../common/Header";
-
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { setHours, setMinutes } from "date-fns";
-
 import calendarImg from "../../assets/images/calendar.png";
 import timeImg from "../../assets/images/timeIcon.png";
 import SaveButton from "../common/SaveButton";
-
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { __getSchedule } from "../../redux/modules/post";
-
 let now = new Date();
 const PostForm = () => {
   const dispatch = useDispatch();
@@ -21,11 +17,9 @@ const PostForm = () => {
   // if (startTime) {
   //   console.log(startTime.getHours());
   // }
-
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
   const [schedule, setSchedule] = useState([]);
-
   // 시작날짜 선택시 습관이 며칠짜리인지에 따라 자동으로 범위선택됨
   const dateRange = (update) => {
     const firstDate = new Date(update[0]);
@@ -34,39 +28,33 @@ const PostForm = () => {
     setStartDate(update[0]);
     setEndDate(new Date(lastDate));
   };
-  // console.log(startDate.getDate());
-  // console.log(startDate.getMonth() + 1);
-  // console.log(startTime.getHours());
-  // console.log(startTime.getMinutes());
-  // console.log(endTime.getHours());
-  // console.log(endTime.getMinutes());
-  const starttime = [startTime.getHours() + ":" + startTime.getMinutes()];
-  console.log(starttime);
-  const endtime = [endTime.getHours() + ":" + endTime.getMinutes()];
-  console.log(endtime);
-  const startyear = [startDate.getFullYear()];
-  const startmoth = [
-    startDate.getMonth() + 1 > 9
-      ? now.getMonth() + 1
-      : "0" + (now.getMonth() + 1),
-  ];
-  const startdate = [
-    startDate.getDate() > 9 ? now.getDate() : "0" + now.getDate(),
-  ];
-  const startday = [startyear + "-" + startmoth + "-" + startdate];
-  console.log(startday);
 
+  console.log(startDate)
+  console.log(startTime)
+  console.log(endTime)
+
+
+  // const starttime = [startTime.getHours() + ":" + startTime.getMinutes()];
+  // const endtime = [endTime.getHours() + ":" + endTime.getMinutes()];
+  // const startyear = [startDate.getFullYear()];
+  // const startmoth = [
+  //   startDate.getMonth() + 1 > 9
+  //     ? now.getMonth() + 1
+  //     : "0" + (now.getMonth() + 1),
+  // ];
+  // const startdate = [
+  //   startDate.getDate() > 9 ? now.getDate() : "0" + now.getDate(),
+  // ];
+  // const startday = [startyear + "-" + startmoth + "-" + startdate];
   // const savePost = () => {
-  //   setSchedule([]);
+  //   setSchedule([...startday, ...starttime, ...endtime]);
   //   dispatch(__getSchedule);
   // };
-
   return (
     <div>
       <Header text={"데일리 설정"} />
       <BodyContainer>
         <div className="tagTitle"> 운동하기 ( 30일 )</div>
-
         <div className="startDate">
           <span className="startDateText">날짜 설정</span>
           <DatePicker
@@ -118,7 +106,6 @@ const PostForm = () => {
             />
           </div>
         </div>
-
         <div className="repeatDay">
           <span className="repeatDayText">반복요일</span>
           <div className="repeatDayArea">
@@ -156,25 +143,22 @@ const PostForm = () => {
       <ButtonContainer>
         <SaveButton
           btnName={"저장"}
-          //  onClick={savePost()}
+          // onClick={savePost()}
         />
       </ButtonContainer>
     </div>
   );
 };
-
 export default PostForm;
-
 const BodyContainer = styled.div`
   /* justify-content: center; */
   display: flex;
   flex-direction: column;
   align-items: center;
-
   //선택한 습관
   & .tagTitle {
     /* 보라그라데이션 */
-    background: linear-gradient(197.06deg, #907cf9 -6.2%, #6334ff 101.13%);
+    background: linear-gradient(197.06deg, #907CF9 -6.2%, #6334FF 101.13%);
     box-shadow: 4px 4px 6px rgba(0, 0, 0, 0.08);
     border-radius: 12px 12px 12px 0px;
     /* 안쪽 글씨 */
@@ -183,7 +167,7 @@ const BodyContainer = styled.div`
     font-size: 16px;
     line-height: 19px;
     text-align: center;
-    color: #ffffff;
+    color: #FFFFFF;
     /* 습관박스 css */
     display: flex;
     justify-content: center;
@@ -209,24 +193,21 @@ const BodyContainer = styled.div`
       background-image: url(${calendarImg});
       background-repeat: no-repeat;
       background-position: center left 3px;
-
       width: 100%;
       height: 42px;
       box-sizing: border-box;
       padding-left: 25px;
       /* margin: 10px; */
-      /* background: #e2e2e2; */
+      /* background: #E2E2E2; */
       border-radius: 8px;
       margin-top: 8px;
     }
   }
-
   & .setTime {
     display: flex;
     width: 100%;
     padding: 0 20px;
     margin-top: 24px;
-
     & .startTime {
       display: flex;
       flex-direction: column;
@@ -244,13 +225,12 @@ const BodyContainer = styled.div`
         background-image: url(${timeImg});
         background-repeat: no-repeat;
         background-position: center left 5px;
-
         width: 100%;
         height: 42px;
         box-sizing: border-box;
         padding-left: 25px;
         /* margin: 12px 20px; */
-        /* background: #e2e2e2; */
+        /* background: #E2E2E2; */
         border-radius: 8px;
         margin-top: 8px;
       }
@@ -271,21 +251,19 @@ const BodyContainer = styled.div`
         background-image: url(${timeImg});
         background-repeat: no-repeat;
         background-position: center left 5px;
-
         width: 100%;
         height: 42px;
         box-sizing: border-box;
         padding-left: 25px;
         /* margin: 12px 20px; */
-        /* background: #e2e2e2; */
+        /* background: #E2E2E2; */
         border-radius: 8px;
         margin-top: 8px;
       }
     }
   }
-
   & .repeatDay {
-    /* background-color: #907cf9; */
+    /* background-color: #907CF9; */
     /* margin: 20px 20px; */
     width: 100%;
     padding: 0 20px;
@@ -297,14 +275,14 @@ const BodyContainer = styled.div`
       line-height: 19px;
     }
     & .repeatDayArea {
-      /* background-color: #eeeeee; */
+      /* background-color: #EEEEEE; */
       margin-top: 8px;
       display: flex;
       & #sunLabel {
         display: flex;
         justify-content: center;
         align-items: center;
-        background-color: #ebebeb; //미클릭시 배경 초기색
+        background-color: #EBEBEB; //미클릭시 배경 초기색
         /* color: #fff; */
         color: black;
         border: none;
@@ -321,7 +299,6 @@ const BodyContainer = styled.div`
           margin: 0;
         }
       }
-
       & .sunCheck {
         display: none;
         position: absolute;
@@ -334,15 +311,14 @@ const BodyContainer = styled.div`
         &:checked + #sunLabel {
           background: linear-gradient(
             197.06deg,
-            #907cf9 -6.2%,
-            #6334ff 101.13%
+            #907CF9 -6.2%,
+            #6334FF 101.13%
           );
         }
       }
     }
   }
 `;
-
 const ButtonContainer = styled.div`
   position: absolute;
   bottom: 32px;
