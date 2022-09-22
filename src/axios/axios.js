cc6147652f22f26b35c24307bcc53f2b0c2e3289
Jisu Lee <jisu2281l@naver.com>
2022-09-23 01:48:10 +0900

    local HTTPS 적용

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -4,13 +4,9 @@ const instance = axios.create({
     baseURL: process.env.REACT_APP_ENDPOINT,
     headers: {
         "Content-Type" : "application/json",
-        "session":"Irr0xUjWFkvTDQU_2Etga3K62LS1MEXu"
-
     },
     timeout: 2000,
 })
 
 
 export default instance
-
-instance.defaults.withCredentials = true;
