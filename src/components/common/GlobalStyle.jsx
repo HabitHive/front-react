9ad29126a596548c12b041444f37bf085699993f
Jisu Lee <jisu2281l@naver.com>
2022-09-28 23:32:00 +0900

    #180 레이아웃 수정
    레이아웃 width
    배경화면 색상 추가

--- src/components/common/GlobalStyle.jsx
+++ src/components/common/GlobalStyle.jsx
@@ -7,6 +7,7 @@ const GlobalStyle = createGlobalStyle`
     }
     body{
         font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+        background-color: #E2DCFF;
     }
     ::-webkit-scrollbar {
         display: none;
